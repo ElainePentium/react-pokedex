cd5cb3a4eede93b6f0cf2460aaeae3f92d4d9d8c
ElainePentium <gentil.stephanie@gmail.com>
2023-05-09 16:55:45 +0200

    PokemonCard added

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,11 @@
+function MyTitle() {
+    return <figure>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
+        <figcaption>Bulbasaur</figcaption>
+    </figure>;
+  }
+  
+  export default MyTitle;
+
+//   Ce composant doit contenir une balise figure qui affiche l'image située ici (tu peux utiliser cette URL directement, pas besoin de télécharger l'image) et qui a comme nom (figcaption) "bulbasaur".
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png
