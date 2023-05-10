7b667228d75bd14ba13c2165c21194ce25bbe595
ElainePentium <gentil.stephanie@gmail.com>
2023-05-10 09:56:09 +0200

    ternary operator + no image for mew

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,23 @@
-function MyTitle() {
-    return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-        <figcaption>Bulbasaur</figcaption>
-    </figure>;
-  }
-  
-  export default MyTitle;
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+];
 
-//   Ce composant doit contenir une balise figure qui affiche l'image située ici (tu peux utiliser cette URL directement, pas besoin de télécharger l'image) et qui a comme nom (figcaption) "bulbasaur".
-// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png
+function PokemonCard() {
+    const pokemon = pokemonList[1];
+
+    return (
+        <figure>
+            {pokemon.imgSrc == undefined ? <p>???</p> : <img src={pokemon.imgSrc} alt={pokemon.name} />}
+            <figcaption>{pokemon.name}</figcaption>
+        </figure>
+    );
+}
+
+export default PokemonCard;
