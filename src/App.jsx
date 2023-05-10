228c7a78ef635e31c1da0b1364fd9187881dc5c7
ElainePentium <gentil.stephanie@gmail.com>
2023-05-10 10:49:38 +0200

    props

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import PokemonCard from "./components/PokemonCard"
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
 function App() {
+
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  
   return (
     <div>
       <PokemonCard pokemon={pokemonList[0]}/>
